7c133ef7f9f330ff831f8b806db07883fae0a166
Raymond Phu Nguyen <phu.nguyen@hextrust.com>
2024-11-20 09:03:59 +0700

    feat: nvimtree relative number false

--- lua/rpn/plugins/nvim-tree.lua
+++ lua/rpn/plugins/nvim-tree.lua
@@ -11,7 +11,7 @@ return {
 		nvimtree.setup({
 			view = {
 				width = 35,
-				relativenumber = true,
+				relativenumber = false,
 				adaptive_size = true,
 			},
 			-- change folder arrow icons
