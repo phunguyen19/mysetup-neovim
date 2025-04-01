019eb7af63d1322fb79e32316c040f8b8779d53f
Raymond Phu Nguyen <phu.nguyen@hextrust.com>
2025-04-01 15:54:40 +0700

    feat: go to tab number

--- lua/rpn/core/keymaps.lua
+++ lua/rpn/core/keymaps.lua
@@ -30,6 +30,12 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+vim.keymap.set("n", "<leader>t", function()
+	local tab_number = tonumber(vim.fn.getcharstr()) -- Get user input for tab number
+	if tab_number then
+		vim.cmd(tab_number .. "tabnext") -- Execute tab switch command
+	end
+end, { desc = "Go to tab position (press number after)" })
 
 -- Copy file relative
 keymap.set(
