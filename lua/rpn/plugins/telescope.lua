d286a138446bc5f5eed6d9fbce81080ea86e7c46
Raymond Phu Nguyen <phu.nguyen@hextrust.com>
2025-04-10 22:51:43 +0700

    chore: adding comments

--- lua/rpn/plugins/telescope.lua
+++ lua/rpn/plugins/telescope.lua
@@ -31,16 +31,20 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		-- Telescope files, jump
+		-- Files
 		local fuzzyhiddencmd = "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files,-u<cr>"
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fu", fuzzyhiddencmd, { desc = "Fuzzy incl. hidden and ignore" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy recent files" })
+
+		-- Jump
 		keymap.set("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "Find jump list" })
+
+		-- Quickfix list
 		keymap.set("n", "<leader>fqq", "<cmd>Telescope quickfix<cr>", { desc = "Find quickfix" })
 		keymap.set("n", "<leader>fqh", "<cmd>Telescope quickfixhistory<cr>", { desc = "Find quickfix history" })
 
-		-- Telescope grep
+		-- Grep
 		local livegrepargs = ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
 		keymap.set("n", "<leader>fgg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fga", livegrepargs, { desc = "live_grep_args" })
@@ -49,12 +53,12 @@ return {
 		keymap.set("n", "<leader>fgs", "<cmd>Telescope git_status<cr>", { desc = "Telescope git_status" })
 		keymap.set("n", "<leader>fgb", "<cmd>Telescope git_bcommits<cr>", { desc = "Telescope git_bcommits" })
 
-		-- Telescope buffer
+		-- Buffer
 		local fuzzycurrbuf = "<cmd>Telescope current_buffer_fuzzy_find<cr>"
 		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
 		keymap.set("n", "<leader>fc", fuzzycurrbuf, { desc = "Fuzzy current buffer" })
 
-		-- Telescope todo
+		-- Todo
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
