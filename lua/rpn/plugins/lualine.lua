d967c935c2f4f01f29fa52fc0a2992654949199a
phunguyen19 <phu.nguyen19@gmail.com>
2026-03-12 13:30:46 +0700

    feat: temporary disable lualine

--- lua/rpn/plugins/lualine.lua
+++ lua/rpn/plugins/lualine.lua
@@ -1,25 +1,28 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		-- configure lualine with modified theme
-		lualine.setup({
-			sections = {
-				lualine_c = { { "filename", path = 1 } },
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
-			},
-		})
-	end,
+	-- "nvim-lualine/lualine.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- config = function()
+	-- 	local lualine = require("lualine")
+	-- 	-- local lazy_status = require("lazy.status")
+	--
+	-- 	-- configure lualine with modified theme
+	-- 	lualine.setup({
+	-- 		sections = {
+	-- 			-- lualine_b = { "branch" },
+	-- 			lualine_b = {},
+	-- 			lualine_c = { { "filename", path = 1 } },
+	-- 			-- lualine_x = {
+	-- 			-- 	{
+	-- 			-- 		lazy_status.updates,
+	-- 			-- 		cond = lazy_status.has_updates,
+	-- 			-- 		color = { fg = "#ff9e64" },
+	-- 			-- 	},
+	-- 			-- 	{ "encoding" },
+	-- 			-- 	{ "fileformat" },
+	-- 			-- 	{ "filetype" },
+	-- 			-- },
+	-- 			lualine_x = {},
+	-- 		},
+	-- 	})
+	-- end,
 }
