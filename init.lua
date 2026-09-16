373b38dce8effca7d0af00fd91e640848a396a44
phunguyen19 <phu.nguyen19@gmail.com>
2026-09-16 12:18:43 +0700

    fix: colorscheme change back to melange

--- init.lua
+++ init.lua
@@ -11,15 +11,15 @@ end
 
 vim.o.termguicolors = true
 
-vim.cmd("colorscheme bamboo")
+vim.cmd("colorscheme melange")
 
 -- Make background transparent for the color schemes
 -- that doesn't support transparent
--- vim.cmd([[
---   hi Normal       guibg=NONE ctermbg=NONE
---   hi NormalNC     guibg=NONE ctermbg=NONE
---   hi Pmenu        guibg=NONE ctermbg=NONE
---   hi SignColumn   guibg=NONE ctermbg=NONE
---   hi VertSplit    guibg=NONE ctermbg=NONE
---   hi StatusLineNC guibg=NONE ctermbg=NONE
--- ]])
+vim.cmd([[
+  hi Normal       guibg=NONE ctermbg=NONE
+  hi NormalNC     guibg=NONE ctermbg=NONE
+  hi Pmenu        guibg=NONE ctermbg=NONE
+  hi SignColumn   guibg=NONE ctermbg=NONE
+  hi VertSplit    guibg=NONE ctermbg=NONE
+  hi StatusLineNC guibg=NONE ctermbg=NONE
+]])
